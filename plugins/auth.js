b51cb601a9f498197423b1b5d08349a2d250eb4d
bussury Omary <bussury@bussurys-MacBook-Pro.local>
2023-06-27 16:17:11 +0300

    app:readme

--- plugins/auth.js
+++ plugins/auth.js
@@ -6,11 +6,17 @@ import fastifyJwt from '@fastify/jwt'
  * @param {import('fastify').FastifyInstance} fastify 
  */
 async function appAuthentication(fastify,opts)  {
-    fastify.decorate('authenticate', async function (){
-
+    fastify.decorate('authenticate', async function (request, reply){
+        try {
+            if(!request.session.userId){
+                
+            }
+        } catch (error) {
+            
+        }
     })
     fastify.decorate('authorize', async function () {
-        
+
     })
    
 }
